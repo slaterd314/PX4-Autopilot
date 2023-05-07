b90e0da1cb0aad2840be647b7250c5648861bc82
Daniel R. Slater <slaterd314@gmail.com>
2023-05-07 15:15:21 -0400

    Added new tetedatics/kimchi board directory based on the nxp/fmuk66-v3 directory. Added openamp library to the PX4 build, which enabled rpmsg on the RTOS side. Currently, CONFIG_ARCH_LOWPUTC is not set, meaning that syslog will currently, not output anything. Enabling CONFIG_ARCH_LOWPUTC implies that we need to define an up_putc(int ch) function.

---
+++ boards/teledatics/kimchi/kimchi.cmake
@@ -0,0 +1,7 @@
+# set (CMAKE_SYSTEM_PROCESSOR "arm")
+# set (MACHINE                "template")
+# # set (CROSS_PREFIX           "arm-none-eabi-" CACHE STRING "")
+# message("CMAKE_SYSTEM_PROCESSOR__0 ${CMAKE_SYSTEM_PROCESSOR}")
+#
+# include_directories(${PX4_SOURCE_DIR}/platforms/nuttx/NuttX/nuttx/include)
+
